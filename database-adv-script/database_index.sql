8f66063be75186c999c66a584b989355e893eca2
AbuArwa001 <khalfanathman12@yahoo.com>
2025-05-17 04:27:51 +0300

    Refactor SQL query formatting for improved readability and maintainability; ensure consistent use of CTEs and EXPLAIN statements.

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,25 +1,14 @@
 
-EXPLAIN WITH CTE_BOOKINGS AS (
-    SELECT
-        property_id,
-        COUNT(booking_id) AS book_count
-    FROM
-        AirBnB.Booking
-    GROUP BY
-        property_id
-)
-SELECT
-    pr.name,
-    BK.property_id,
-    pr.host_id,
-    pr.price_per_night,
-    COALESCE(BK.book_count, 0) AS number_of_bookings,
-    ROW_NUMBER() OVER (ORDER BY BK.book_count DESC) AS PROPERTY_ROWS,
-    RANK() OVER (ORDER BY BK.book_count DESC) AS PROPERTY_RANK
-FROM
-    Property pr
-LEFT JOIN CTE_BOOKINGS BK ON BK.property_id = pr.property_id
-;
+EXPLAIN (WITH CTE_BOOKINGS AS (SELECT property_id, COUNT(booking_id) AS book_count FROM AirBnB.Booking GROUP BY property_id )
+SELECT pr.name, BK.property_id, pr.host_id, pr.price_per_night, COALESCE(BK.book_count, 0) AS number_of_bookings, ROW_NUMBER() OVER (ORDER BY BK.book_count DESC) AS PROPERTY_ROWS, RANK() OVER (ORDER BY BK.book_count DESC) AS PROPERTY_RANK FROM Property pr LEFT JOIN
+CTE_BOOKINGS BK ON BK.property_id = pr.property_id)  \G
+
+
+EXPLAIN SELECT * FROM Property P WHERE (SELECT AVG(R.rating) FROM Review R WHERE P.property_id = R.property_id) > 4.0 \G
+--
+
+-- This script creates indexes on the tables in the AirBnB database to improve query performance.
+
 
 CREATE INDEX idx_user_email ON User(email);
 CREATE INDEX idx_property_host ON Property(host_id);
